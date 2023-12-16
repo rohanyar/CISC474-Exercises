d34dd41c37fbdcd4bd83a32d75dc2488a702c475
rohanyar <99371548+rohanyar@users.noreply.github.com>
2023-12-16 16:43:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function() {
+    console.log("ready!");
+
+    $("#go").click(function(event){
+        event.preventDefault();
+        var fn = $("#fname").val();
+        var ln = $("#lname").val();
+        $("#jumbotron").text(fn + ' ' + ln);
+      });
 });
